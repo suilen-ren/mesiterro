9e01b4055c840b9cf16ad9785f4c347b77213954
EC2 Default User <ec2-user@ip-172-31-30-48.ec2.internal>
2022-05-04 02:31:40 +0000

    [Finish] 4-11

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,12 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('apps/assert/images/no_image.jpg')
+      image.attach(io:File.poen(file_path), filename: "defaulf-image.jpg",content_type: "image/jpeg")
+    end
+    image
+  end
 end
